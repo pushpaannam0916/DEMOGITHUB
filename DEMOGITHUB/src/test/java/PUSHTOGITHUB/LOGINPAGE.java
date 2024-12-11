9d01b73fc7f4ec7f34c3413da4bd9df92bee57ae
pushpaannam0916 <pushpaannam0916@gmail.com>
2024-12-11 07:18:20 +0530

    commit 3

--- DEMOGITHUB/src/test/java/PUSHTOGITHUB/LOGINPAGE.java
+++ DEMOGITHUB/src/test/java/PUSHTOGITHUB/LOGINPAGE.java
@@ -1,16 +1,18 @@
 package PUSHTOGITHUB;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+
+
 
 public class LOGINPAGE {
-	@FindBy(id="email")//Annotation
-	private WebElement Email;//Encapsulation
 	
-	@FindBy(id="password")
-	private WebElement Password; //(password)
-	l
-	cvb//
+	public static void main(String[] args)
+	{
+		System.out.println("1");
+		System.out.println("2");
+		
+		
+	}
+	
 
 }
 
